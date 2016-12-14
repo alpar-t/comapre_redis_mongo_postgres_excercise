5b62f2dfc6907caa14cb7c0f269b035d8db2a83b
Alpar Torok <torokalpar@gmail.com>
2016-12-14 23:40:17 +0200

    Add smarts to the import script so it won't owerwrite
    Sample prefixes are checked to see if the script was ran before

--- import_legacy.py
+++ import_legacy.py
@@ -30,12 +30,24 @@ def parse_args():
     return parser.parse_args()
 
 
+def check_for_existing(rule_ops, samples):
+    return any((
+        rule_ops.query_rule(prefix) is not None for prefix in samples
+    ))
+
+
 def main():
     args = parse_args()
     rule_ops = RuleOperations(
         redis.StrictRedis(host=args.host, port=args.port)
     )
-    for prefix in phone_legacy_data.RESTRICTED_OUTBOUND_PAYING_PREFIXES.keys():
+    generic_restrictions = sorted(phone_legacy_data.RESTRICTED_OUTBOUND_PAYING_PREFIXES.keys())
+
+    if check_for_existing(rule_ops, (generic_restrictions[0], generic_restrictions[10])):
+        print("Rules were already laoded. Refusign to do any changes")
+        return
+
+    for prefix in generic_restrictions:
         rule_ops.push_generic_rule(prefix, rule_ops.R_RESTRICT)
         # add the rule as a prefix rule as well to speed up queries for trial
         # users.
@@ -43,12 +55,14 @@ def main():
         # memory
         rule_ops.push_trial_rule(prefix, rule_ops.R_RESTRICT)
     print("Imported {} general rules".format(
-        len(phone_legacy_data.RESTRICTED_OUTBOUND_PAYING_PREFIXES.keys())
+        len(generic_restrictions)
     ))
-    for prefix in phone_legacy_data.RESTRICTED_OUTBOUND_TRIAL_PREFIXES.keys():
+
+    trial_restrictions=phone_legacy_data.RESTRICTED_OUTBOUND_TRIAL_PREFIXES.keys()
+    for prefix in trial_restrictions:
         rule_ops.push_trial_rule(prefix, rule_ops.R_RESTRICT)
     print("Imported {} trial rules".format(
-        len(phone_legacy_data.RESTRICTED_OUTBOUND_TRIAL_PREFIXES.keys())
+        len(trial_restrictions)
     ))
 
 if __name__ == "__main__":
